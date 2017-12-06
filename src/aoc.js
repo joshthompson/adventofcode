fb7290006a9552c27792e9dcdfcfe7885bdf0e50
Josh Thompson <josh3276@gmail.com>
2017-12-06 09:12:11 +0100

    Day 6

--- src/aoc.js
+++ src/aoc.js
@@ -182,6 +182,30 @@ var aoc =  [
 				return steps
 			}
 		]
+	},
+	{
+		// DAY 6
+		parts: [
+			input => {
+				let bank = input.split('\t').map(i => parseInt(i))
+				let tracking = {}
+				let remember = () => tracking[bank.join()] ? false : tracking[bank.join()] = steps
+				let steps = 0
+
+				while (remember() !== false) {
+					let highest = Math.max.apply(null, bank)
+					let index = bank.indexOf(highest)
+					let value = bank[index]
+					bank[index] = 0
+					for (let i = 1; i <= value; i++) {
+						bank[(index + i) % bank.length]++
+					}
+					steps++
+				}
+				let diff = steps - tracking[bank.join()]
+				return `${steps}<br/>Part 2: ${diff}` // Easier just to do in one function
+			}
+		]
 	}
 ]
 
