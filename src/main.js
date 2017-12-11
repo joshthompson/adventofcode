380a6a37e3301104fd1cebf4abd65421071b9a93
Josh Thompson <josh3276@gmail.com>
2017-12-11 14:28:41 +0100

    Design

--- src/main.js
+++ src/main.js
@@ -8,12 +8,15 @@ for (let day = 1; day <= 11; day++) {
 var AdventOfCode = {}
 
 let render = async (i) => {
-	document.getElementById('results').innerHTML = `DAY ${i} RESULTS:<br/><br/>`
+	document.getElementById('results').innerHTML = `DAY ${i} RESULTS:<br/><br/><div id='day'><div id='parts'></div><code id='code' class='prettyprint'></code></div>`
 	let day = i < 10 ? '0' + i : i
 	if (typeof AdventOfCode[`day${day}`] === 'undefined') {
 		await fetch(`/${year}/day${day}.js`)
 			.then(response => response.text())
-			.then(js => eval(js))
+			.then(js => {
+				eval(js)
+				document.getElementById('code').innerHTML = js
+			})
 	}
 
 	await fetch(`/${year}/day${day}.txt`)
@@ -25,16 +28,11 @@ let render = async (i) => {
 				document.getElementById('results').innerHTML += `<div>This day's tasks take a while to run...</div>`
 			}
 
-			setTimeout(() => {
-				[1, 2].forEach((i) => {
-					let part = AdventOfCode[`day${day}`][`part${i}`]
-					var partElement = document.createElement('div')
-						partElement.classList = ['part']
-						partElement.innerHTML = `<div>Part ${i}: ${part(input)}</div>`
-						document.getElementById('results').appendChild(partElement)
-					}
-				)
-			}, 10)
+			document.getElementById('parts').innerHTML = ''
+			for (let part = 1; part <= 2; part++) {
+				let result = AdventOfCode[`day${day}`][`part${part}`](input)
+				document.getElementById('parts').innerHTML += `<div>Part ${part}: ${result}</div>`
+			}
 		})
 
 }
