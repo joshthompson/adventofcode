376b98aaf0dfc02fa71f95b24e85a9a0d1ff5a1e
Code Build <josh3276@gmail.com>
2021-12-03 00:51:29 +0100

    Solution for day 1 and 2 (2021)

---
+++ 2021/01/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs')
+
+fs.readFile(`${__dirname}/input.txt`, (_e, data) => {
+	const readings = data.toString().split('\n').map(Number)
+	console.log('Part 1', part1(readings))
+	console.log('Part 2', part2(readings))
+})
+
+function part1(readings) {
+  let increases = 0;
+  for (let i = 1; i < readings.length; i++) {
+    if (readings[i] > readings[i - 1]) {
+      increases++
+    }
+  }
+  return increases
+}
+
+function part2(readings) {
+  let increases = 0;
+  for (let i = 3; i < readings.length; i++) {
+    const prev = readings[i - 3] + readings[i - 2] + readings[i - 1];
+    const curr = readings[i - 2] + readings[i - 1] + readings[i];
+    if (curr > prev) {
+      increases++
+    }
+  }
+  return increases
+}
---
+++ 2021/02/index.js
@@ -0,0 +1,54 @@
+const fs = require('fs')
+
+fs.readFile(`${__dirname}/input.txt`, (_e, data) => {
+	const commands = data.toString().split('\n').map(SubCommand)
+	console.log('Part 1', part1(commands))
+	console.log('Part 2', part2(commands))
+})
+
+function part1(commands) {
+  let depth = 0
+  let horiz = 0
+  commands.forEach(({ command, value }) => {
+    switch (command) {
+      case 'forward':
+        horiz += value
+        break
+      case 'up':
+        depth -= value
+        break
+      case 'down':
+        depth += value
+        break
+    }
+  })
+  return depth * horiz
+}
+
+function part2(commands) {
+  let depth = 0
+  let horiz = 0
+  let aim = 0
+  commands.forEach(({ command, value }) => {
+    switch (command) {
+      case 'forward':
+        horiz += value
+        depth += aim * value
+        break
+      case 'up':
+        aim -= value
+        break
+      case 'down':
+        aim += value
+        break
+    }
+  })
+  return depth * horiz
+}
+
+function SubCommand(input) {
+  return {
+    command: input.split(' ')[0],
+    value: parseInt(input.split(' ')[1]),
+  }
+}
